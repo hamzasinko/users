8e17bff2e4100860ef079e6b5d4fa6a5e4f61eac
unknown <hamzawin2018@gmail.com>
2024-05-15 12:08:56 +0100

    first commit

---
+++ src/main/java/ma/enset/jpaenset/entities/Role.java
@@ -0,0 +1,20 @@
+package ma.enset.jpaenset.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Role {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String desc;
+    @Column(length = 20, unique = true)
+    private String roleName;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<User> users = new ArrayList<>() ;
+}
---
+++ src/main/java/ma/enset/jpaenset/service/UserService.java
@@ -0,0 +1,12 @@
+package ma.enset.jpaenset.service;
+
+import ma.enset.jpaenset.entities.Role;
+import ma.enset.jpaenset.entities.User;
+
+public interface UserService {
+    User addNewUser(User user);
+    Role addNewRole(Role role);
+    User findUserByUserName(String userName);
+    Role findRoleByRoleName(String roleName);
+    void addRoleToUser(String username, String roleName);
+}
